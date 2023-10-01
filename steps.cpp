cb3bd2eb66fdd1144ee9a1676f47bc60a2609db5
Shady Atef <shady_atef@dell.com>
2023-10-01 08:52:44 -0400

    Added steps.cpp

--- steps.cpp
+++ steps.cpp
@@ -1,1 +1,22 @@
-int main
+bool IsEven(int n) {
+    return n%2 ==0;
+}
+
+int incrementOnlyEven(int n)
+{
+
+    if (IsEven(n))
+        n += 1;
+
+    return n;
+}
+
+int main()
+{
+    int x = 0;
+    x+=2;
+    
+    x = incrementOnlyEven(x);
+
+    return 0;
+}
