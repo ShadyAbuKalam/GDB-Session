16c256d50cb3f4607c7c93327f2f28bbe31687aa
Shady Atef <shady_atef@dell.com>
2023-10-02 11:36:52 -0400

    Added linker error

---
+++ linker_error/main.cpp
@@ -0,0 +1,6 @@
+#include "my_f.h"
+int main()
+{
+    f();
+    return 0;
+}
---
+++ linker_error/my_f.cpp
@@ -0,0 +1,7 @@
+#include "my_f.h"
+#include<iostream>
+
+void f2()
+{
+    std::cout << "Wonder why it doesn't compile" << std::endl;
+}
